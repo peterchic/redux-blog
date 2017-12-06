756db4a08924cd0e27576689765bdba8e7f1f5de
peterchic <peterchicarielli@gmail.com>
2017-12-05 19:12:16 -0500

    wired up show component to reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,20 @@
 import _ from 'lodash'
 import { FETCH_POSTS } from '../actions'
+import { FETCH_POST } from '../actions'
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+
+    //ES5
+    // const post = action.payload.data
+    // const newState = { ...state}
+    // newState[post.id] = post
+    // return newState
+
+    //ES6
+
+    return { ...state, action.payload.data}
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id')
   default:
